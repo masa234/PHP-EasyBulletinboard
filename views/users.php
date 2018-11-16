583e25af34927906dc8d7a0721b88ce85e92dee0
tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-11-17 01:50:51 +0900

    change include to require

--- views/users.php
+++ views/users.php
@@ -1,11 +1,11 @@
 <?php
 
 require (  "../setting_func.php" );
-include ( get_require_dir() . "/dbconfig.php" );
-include ( get_require_dir() . "/common.php" );
-include ( get_require_dir() . "/session.php" );
-include ( get_require_dir() . "/user.php" );
-include ( get_require_dir() . "/navbar.php" );
+require ( get_require_dir() . "/dbconfig.php" );
+require ( get_require_dir() . "/common.php" );
+require ( get_require_dir() . "/session.php" );
+require ( get_require_dir() . "/user.php" );
+require ( get_require_dir() . "/navbar.php" );
 
 $users = get_all( 'users', 'id', 'DESC' );
 
