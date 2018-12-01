39e66424d1b2635c66b12a5321c9a45f1a3965df
tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-12-01 22:56:46 +0900

    common.php add many function

--- create_table.sql
+++ create_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018 年 11 朁E16 日 17:32
+-- Generation Time: 2018 年 11 朁E30 日 20:16
 -- サーバのバージョン： 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -25,6 +25,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `followings`
+--
+
+CREATE TABLE `followings` (
+  `user_id` int(255) NOT NULL,
+  `followed_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `followings`
+--
+
+INSERT INTO `followings` (`user_id`, `followed_id`) VALUES
+(16, 20),
+(20, 30),
+(21, 20);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `post_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `likes`
+--
+
+INSERT INTO `likes` (`id`, `user_id`, `post_id`) VALUES
+(40, 20, 3);
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `posts`
 --
 
@@ -39,6 +78,27 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- テーブルのデータのダンプ `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `image`, `created_at`, `updated_at`, `user_id`) VALUES
+(3, 'f', 'd', '20181127031044356a192b7913b04c54574d18c28d46e6395428ab.jpeg', '2018-11-27 03:10:44', '2018-11-27 03:10:44', 16);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `retweets`
+--
+
+CREATE TABLE `retweets` (
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `post_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `users`
 --
 
@@ -47,18 +107,45 @@ CREATE TABLE `users` (
   `user_name` varchar(50) NOT NULL,
   `nickname` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `admin` tinyint(2) NOT NULL,
   `image` text NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `users`
+--
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `followings`
+--
+ALTER TABLE `followings`
+  ADD UNIQUE KEY `user_id_2` (`user_id`,`followed_id`),
+  ADD KEY `user_id` (`user_id`,`followed_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD KEY `id` (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `retweets`
+--
+ALTER TABLE `retweets`
+  ADD KEY `post_id` (`post_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -72,73 +159,54 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- ダンプしたテーブルの制約
 --
 
 --
--- テーブルの制約 `posts`
+-- テーブルの制約 `followings`
 --
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-COMMIT;
-
---
--- テーブルの構造 `likes`
---
-
-CREATE TABLE `likes` (
-  `id` int(255) NOT NULL,
-  `user_id` int(255) NOT NULL,
-  `post_id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD KEY `id` (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `post_id` (`post_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- ダンプしたテーブルの制約
---
+ALTER TABLE `followings`
+  ADD CONSTRAINT `followings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
 
 --
 -- テーブルの制約 `likes`
 --
 ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+
+--
+-- テーブルの制約 `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+
+--
+-- テーブルの制約 `retweets`
+--
+ALTER TABLE `retweets`
+  ADD CONSTRAINT `retweets_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+  ADD CONSTRAINT `retweets_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
